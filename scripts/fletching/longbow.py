6b0c7e803cbceec63a21dc238f46fc3df8cb7874
James <jamessorrell95@gmail.com>
2020-07-03 12:09:46 +1000

    pushing local changes

--- scripts/fletching/longbow.py
+++ scripts/fletching/longbow.py
@@ -13,30 +13,30 @@ print("Looping {} times.".format(loop))
 
 b = OSBrain()
 
-CHEST_LOC = (249, 99, 300, 134)
+CHEST_LOC = (281, 124, 345, 162)
 b.per.moveToBox(CHEST_LOC, 'medium')
 b.per.click('left')
-t.randomSleep(160, 15)
+t.randomSleep(500, 15)
 
 CHEST_CLR1 = (77, 49, 31)
 CHEST_CLR2 = (72, 45, 28)
 
 for _ in range(loop):
 
-    LOGS_LOC = (84, 119, 96, 131)
+    LOGS_LOC = (104, 150, 123, 168)
 
     b.per.moveToBox(LOGS_LOC, 'medium')
     b.per.click('left')
     t.randomSleep(160, 15)
 
-    BANK_X = (485, 44, 493, 53)
+    BANK_X = (607, 55, 616, 70)
 
     b.per.moveToBox(BANK_X, 'medium')
     b.per.click('left')
     t.randomSleep(160, 15)
 
-    KNIFE_LOC = (573, 247, 585, 263)
-    INV_LOC = (612, 247, 631, 263)
+    KNIFE_LOC = (714, 307, 731, 323)
+    INV_LOC = (769, 307, 788, 323)
 
     b.per.moveToBox(KNIFE_LOC, 'medium')
     b.per.click('left')
@@ -46,9 +46,11 @@ for _ in range(loop):
     b.per.click('left')
     t.randomSleep(450, 15)
 
-    LONGBOW_LOC = (229, 425, 290, 469)
+    LONGBOW_LOC = (286, 530, 365, 584)
     b.per.moveToBox(LONGBOW_LOC, 'medium')
+    t.randomSleep(200, 15)
     b.per.click('left')
+    t.randomSleep(120, 15)
 
     chest_located = b.detection.colorRangeSearch(CHEST_LOC, CHEST_CLR1, CHEST_CLR2)
     print("CHEST LOCATED: {}".format(chest_located))
@@ -60,7 +62,7 @@ for _ in range(loop):
 
     b.per.moveToBox(CHEST_LOC, 'medium')
     b.per.click('left')
-    t.randomSleep(160, 15)
+    t.randomSleep(500, 15)
 
     b.per.moveToBox(INV_LOC, 'medium')
     b.per.click('left')
